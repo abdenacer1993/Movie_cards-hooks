69b105d0f34b59d13f8849c4c2152dbc43e64196
abdenacer1993 <abdenacer1993@gmail.com>
2022-05-25 14:50:36 +0100

    third commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.css' ;
+
 import MovieList from './components/MovieList';
 import Searsh from './components/Searsh';
 import { data } from './data'
 import { useState } from 'react';
+import Add from './components/Add';
+
 
 
 function App() {
-  const [Moviec, setMoviec] = useState([])
+  const [Moviec, setMoviec] = useState(data)
   const [Moviesearchval, setMoviesearchval] = useState("")
   const [Moviesearchrate, setMoviesearchrate] = useState(0)
   const clickShearchVal = (value) => {setMoviesearchval(value)}
   const clickSearchByRate = (rate) => {setMoviesearchrate(rate)}
   
-  
+  const AddMovie = (movie) => { setMoviec([...Moviec,movie]) }
+
   return (
+    <div>
     <div className="container-fluid movie-app">
       <div className='row'>
       <Searsh clickShearchVal={clickShearchVal} Moviesearchrate={Moviesearchrate} clickSearchByRate={clickSearchByRate}/>
-      <MovieList data={data.filter(
+      <MovieList data={Moviec.filter(
           (el)=>el.Title.toLowerCase().includes(Moviesearchval.trim().toLowerCase())
           &&el.Rate>=Moviesearchrate
         )} />
       
       </div>
+      </div>
+      <Add AddMovie={AddMovie}/>
+
     </div>
   );
 }
--- src/components/Searsh.js
+++ src/components/Searsh.js
@@ -3,7 +3,7 @@ import { Rating } from 'react-simple-star-rating'
 
 function Searsh({clickShearchVal,clickSearchByRate,Moviesearchrate}) {
   return (
-    <div className='search'>
+    <div className='searchnacer'>
         <input className="form__input" type='text' placeholder='Search Movie ...' onChange={(e)=>clickShearchVal(e.target.value)}/>
         <Rating  onClick={clickSearchByRate} Moviesearchrate={Moviesearchrate} />
     </div>
